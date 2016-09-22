5e0325c3501a54e97dd6e3d45af8d33bc61d7481
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2016-09-22 03:54:41 -0500

    make buildable on xpe

--- xpe.mk
+++ xpe.mk
@@ -19,9 +19,9 @@
 $(call inherit-product, device/htc/a5ul/full_a5ul.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/XPe/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/XPe/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_a5ul
+PRODUCT_NAME := xpe_a5ul
